e99445b3b05d30cf755ee6b8d31a8fa2f6958ae2
Jonathan Ballard <jonathan.ballard@gmail.com>
2015-08-29 08:58:08 -0400

    Restrict pry

--- cia.rb
+++ cia.rb
@@ -1,8 +1,8 @@
-require 'pry'
 require_relative './routes/init'
 
 class CIA < Sinatra::Application
-  configure do
+  configure :development do
+    require 'pry'
   end
 
   configure :production do
