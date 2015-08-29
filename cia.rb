14ba06dbf5a97281f252a0b051af716b5240d4fa
Jonathan Ballard <jonathan.ballard@gmail.com>
2015-08-29 18:17:23 -0400

    Add versioning to header.

--- cia.rb
+++ cia.rb
@@ -1,6 +1,11 @@
 require_relative './routes/init'
 
 class CIA < Sinatra::Application
+  configure do
+    set :api_prefix, 'application/vnd.jonathanballard.com'
+    set :api_versions, %w(v1)
+  end
+
   configure :development do
     require 'pry'
   end
